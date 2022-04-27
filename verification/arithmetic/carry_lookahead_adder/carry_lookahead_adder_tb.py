87702829b46f1d2712c0da2499a03890ee4dc205
Michael Geddes <michaelgeddes555@gmail.com>
2022-04-27 12:18:29 -0400

    Updated CLA unit test

--- verification/arithmetic/carry_lookahead_adder/carry_lookahead_adder_tb.py
+++ verification/arithmetic/carry_lookahead_adder/carry_lookahead_adder_tb.py
@@ -1,8 +1,9 @@
 import cocotb
 from cocotb.triggers import Timer
 
+
 @cocotb.test()
-async def test_diff_simple(dut):
+async def test_zeros(dut):
 
     x = 0
     y = 0
@@ -14,28 +15,15 @@ async def test_diff_simple(dut):
 
     await Timer(10, units="us")
 
-    assert dut.sum.value == 0, "Adder result is incorrect"
+    assert dut.sum.value == 0, f"sum {dut.sum.value} is not 0!"
+    assert dut.cout.value == 0, f"cout {dut.cout.value} is not 0!"
+
 
 @cocotb.test()
-async def test_2(dut):
+async def test_add(dut):
 
     x = 5
     y = 6
-    cin = 0
-
-    dut.x.value = x
-    dut.y.value = y
-    dut.cin.value = cin
-
-    await Timer(10, units="us")
-
-    assert dut.sum.value == 11, "Adder result is incorrect"
-
-@cocotb.test()
-async def test_3(dut):
-
-    x = 255
-    y = 0
     cin = 1
 
     dut.x.value = x
@@ -44,5 +32,22 @@ async def test_3(dut):
 
     await Timer(10, units="us")
 
-    assert dut.sum.value == 0, "Adder result is incorrect"
-    assert dut.cout.value == 1, "Adder result is incorrect"
+    assert dut.sum.value == 12, f"sum {dut.sum.value} is not 12!"
+    assert dut.cout.value == 0, f"cout {dut.cout.value} is not 0!"
+
+
+@cocotb.test()
+async def test_overflow(dut):
+
+    x = 255
+    y = 1
+    cin = 1
+
+    dut.x.value = x
+    dut.y.value = y
+    dut.cin.value = cin
+
+    await Timer(10, units="us")
+
+    assert dut.sum.value == 1, f"sum {dut.sum.value} is not 1!"
+    assert dut.cout.value == 1, f"cout {dut.cout.value} is not 1!"
