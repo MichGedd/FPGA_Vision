20c75b6c980650ee70bba814d8a0546690cadbc2
Michael Geddes <michaelgeddes555@gmail.com>
2022-04-30 13:57:50 -0400

    Added tests for RGB2YCBCR.
    Average error is a little over 2 units

--- hdl/rgb_to_ycrcb.v
+++ hdl/rgb_to_ycrcb.v
@@ -7,17 +7,17 @@ module rgb_to_ycrcb(input clk,
 	output reg [7:0] cb);  // 8-bit unsigned
 	
 	// Y constants
-	wire [7:0] const_y_r = 8'h4c;  // 0.299
-	wire [7:0] const_y_g = 8'h96;  // 0.587
-	wire [7:0] const_y_b = 8'h24;  // 0.144
+	wire [7:0] const_y_r = 8'h4c;  // 0.299 (actual is 0.296875)
+	wire [7:0] const_y_g = 8'h96;  // 0.587 (actual is 0.5859375)
+	wire [7:0] const_y_b = 8'h24;  // 0.144 (actual is0.140625)
 	
 	// Cr constants
-	wire [7:0] const_cr_scale = 8'hb6;  // 0.713
+	wire signed [8:0] const_cr_scale = 9'h0b6;  // 0.713 (actual is 0.7109375)
 	
 	// Cb constants
-	wire [7:0] const_cb_scale = 8'h90;  // 0.564
+	wire signed [8:0] const_cb_scale = 8'h090;  // 0.564 (actual is 0.5625)
 	
-	// Multiply
+	// Obtain Y
 	reg [15:0] y_product_r, y_product_g, y_product_b;
 	
 	always @(posedge clk) begin
@@ -26,16 +26,32 @@ module rgb_to_ycrcb(input clk,
 		y_product_b <= b * const_y_b;
 	end
 	
-	// Add
+	// Subtract Y from R and B
 	wire signed [16:0] y_signed = y_product_r + y_product_g + y_product_b;
 	reg signed [16:0] r_sub_y, b_sub_y;
-	reg [7:0] y_buffer;
+	reg [8:0] y_buffer;
 	
 	always @(posedge clk) begin
-		y_buffer <= y_signed[15:8];
+		y_buffer <= y_signed[16:8];
 		r_sub_y <= {1'b0, r, {8{1'b0}}} - y_signed;
 		b_sub_y <= {1'b0, b, {8{1'b0}}} - y_signed;
 	end
+	
+	// Multiply by Cr and Cb constants, fit to range (0-255)
+	wire signed [24:0] cr_product =  r_sub_y * const_cr_scale + (128 << 16);
+	wire signed [24:0] cb_product =  b_sub_y * const_cb_scale + (128 << 16);
+
+	always @(posedge clk) begin
+		cr <= cr_product[23:16];
+		cb <= cb_product[23:16];
+		
+		if(y_buffer > 255) begin
+			y <= 255;
+		end else begin
+			y <= y_buffer[7:0];
+		end
+	end
+	
 	
 	
 endmodule
--- verification/rgb_to_ycrcb/rgb_to_ycrcb_tb.py
+++ verification/rgb_to_ycrcb/rgb_to_ycrcb_tb.py
@@ -1,6 +1,25 @@
 import cocotb
+import random
+import cv2 as cv
+import numpy as np
 from cocotb.clock import Clock
 from cocotb.triggers import FallingEdge
+
+
+class RGB_to_YCbCr:
+    def __init__(self, r, g, b):
+        self.y = 0.296875 * r + 0.5859375 * g + 0.140625 * b
+        self.cr = (r - self.y) * 0.7109375 + 128
+        self.cb = (b - self.y) * 0.5625 + 128
+
+    def get_y(self):
+        return int(self.y) if self.y < 255 else 255
+
+    def get_cr(self):
+        return int(self.cr)
+
+    def get_cb(self):
+        return int(self.cb)
 
 
 @cocotb.test()
@@ -13,10 +32,85 @@ async def test_zeros(dut):
     dut.g.value = 142
     dut.b.value = 116
 
+    ycbcr = RGB_to_YCbCr(57, 142, 116)
+
     await FallingEdge(dut.clk)
     await FallingEdge(dut.clk)
     await FallingEdge(dut.clk)
 
-    print(f'y value is {dut.y.value}')
-    print(f'cr value is {dut.cr.value}')
-    print(f'cb value is {dut.cb.value}')
+    assert dut.y.value == ycbcr.get_y(), f'{dut.y.value} does not equal {ycbcr.get_y()}'
+    assert dut.cb.value == ycbcr.get_cb(), f'{dut.cb.value} does not equal {ycbcr.get_cb()}'
+    assert dut.cr.value == ycbcr.get_cr(), f'{dut.cr.value} does not equal {ycbcr.get_cr()}'
+
+
+@cocotb.test()
+async def test_random_rgb(dut):
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    dut.r.value = r
+    dut.g.value = g
+    dut.b.value = b
+
+    ycbcr = RGB_to_YCbCr(r, g, b)
+
+    await FallingEdge(dut.clk)
+    await FallingEdge(dut.clk)
+    await FallingEdge(dut.clk)
+
+    assert dut.y.value == ycbcr.get_y(), f'{dut.y.value} does not equal {ycbcr.get_y()}'
+    assert dut.cb.value == ycbcr.get_cb(), f'{dut.cb.value} does not equal {ycbcr.get_cb()}'
+    assert dut.cr.value == ycbcr.get_cr(), f'{dut.cr.value} does not equal {ycbcr.get_cr()}'
+
+
+@cocotb.test()
+async def validate_against_opencv(dut):
+
+    errors_y = []
+    errors_cr = []
+    errors_cb = []
+
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    for i in range(0, 256):
+        for j in range(0, 256):
+            for k in range(0, 256):
+                r = i
+                g = j
+                b = k
+
+                rgb = np.uint8([[[r, g, b]]])
+                ycrcb = cv.cvtColor(rgb, cv.COLOR_RGB2YCrCb)
+
+                dut.r.value = r
+                dut.g.value = g
+                dut.b.value = b
+
+                await FallingEdge(dut.clk)
+                await FallingEdge(dut.clk)
+                await FallingEdge(dut.clk)
+
+                error_y = abs(dut.y.value - ycrcb[0][0][0])
+                error_cr = abs(dut.cr.value - ycrcb[0][0][1])
+                error_cb = abs(dut.cb.value - ycrcb[0][0][2])
+
+                errors_y.append(error_y)
+                errors_cr.append(error_cr)
+                errors_cb.append(error_cb)
+
+    sum_y = 0
+    sum_cr = 0
+    sum_cb = 0
+    for i in range(0, len(errors_y)):
+        sum_y += errors_y[i]
+        sum_cr += errors_cr[i]
+        sum_cb += errors_cb[i]
+
+    print(f'Mean absolute error for y is {sum_y / len(errors_y)} units')
+    print(f'Mean absolute error error for cr is {sum_cr / len(errors_cr)} units')
+    print(f'Mean absolute error error for cb is {sum_cb / len(errors_cb)} units')
